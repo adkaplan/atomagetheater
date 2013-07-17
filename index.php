062025701712da74570fbc2edd72ffea0add76ea
adkaplan <adkaplan@virginia.edu>
2013-07-17 00:50:02 -0400

    Why did this go so slow...

--- index.php
+++ index.php
@@ -23,32 +23,66 @@
     });
 </script>
 <div id="masterdiv">
-    <div class="leftTab poptab" style="top:200px;">
-        <div class="tabText unselectable">
-            Home
+    <a href="home">
+        <div id = "home" class="leftTab tab noisy" style="top:200px;" onclick="tabClick(event)">
+            <div class="tabText unselectable">
+                Home
+            </div>
         </div>
-    </div>
-    <div class="leftTab tab" style="top:240px;">
-        <div class="tabText unselectable">
-            Tix
+    </a>
+    <a href="tix">
+        <div id = "tix" class="leftTab tab noisy" style="top:235px;" onclick="tabClick(event)">
+            <div class="tabText unselectable">
+                Tix
+            </div>
         </div>
-    </div>
-    <div class="leftTab tab" style="top:280px;">
-        <div class="tabText unselectable">
-            Crew
+    </a>
+    <a href="crew">
+        <div id = "crew" class="leftTab tab noisy" style="top:270px;" onclick="tabClick(event)">
+            <div class="tabText unselectable">
+                Crew
+            </div>
         </div>
-    </div>
-    <div class="leftTab tab" style="top:320px;">
-        <div class="tabText unselectable">
-            Contact
+    </a>
+    <a href="contact">
+        <div id = "contact" class="leftTab tab noisy" style="top:305px;" onclick="tabClick(event)">
+            <div class="tabText unselectable">
+                Contact
+            </div>
         </div>
-    </div>
+    </a>
+    <script type="text/javascript">
+            $(".leftTab").each( function() {
+                    if(this.id == '<?php echo $id; ?>') {
+                            $(this).removeClass("tab");
+                            $(this).removeClass("noisy");
+                            $(this).addClass("poptab");
+                    };
+            });
+    </script>
+    <a href="http://www.boston.com/yourcampus/news/emerson/2012/12/ambition_passion_drives_emersons_non-profit_student_theater_group.html" onclick="javascript:void window.open('http://www.boston.com/yourcampus/news/emerson/2012/12/ambition_passion_drives_emersons_non-profit_student_theater_group.html','1374036248335','width=700,height=500,toolbar=0,menubar=0,location=0,status=1,scrollbars=1,resizable=1,left=0,top=0');return false;">
+        <div class="rightTab noisy" style="top:200px;" >
+            <div class="tabText unselectable">
+                In the News
+            </div>
+        </div>
+    </a>
 
-    <div id="content" class="noisy">
+    <div id="contenttop" class="noisy">
         <center>
         <img src="img/header.png"/><br>
     	</center>
-        <div class="content" id="<?php echo $id ?>"><?php print file_get_contents($url.'php/load.php?id='.$id) ?></div>
+
+        <div id="contentleft" class="content" id="<?php echo $id ?>"><?php print file_get_contents($url.'php/load.php?id='.$id) ?></div>
+        <div id="contentright">
+            <center><img src="img/ourseason.png"/><br>
+            <?php
+             $input = explode(",",file_get_contents('txt/homeimg.txt'));
+             for ($i = 0;$i<count($input);$i++) {
+                echo "<img src='" . $input[$i] . "' class='.contentrightimage'/><br>";
+             }
+             ?>
+        </div>
     </div>
     <div id="gutterL">
         <img src="img/cornerL.gif" class="top"/>
